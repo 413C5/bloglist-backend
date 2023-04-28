1607492758e651ba5d16aa1299f2b730d9aab564
413C5 <a_gonzalezzarate2@yahoo.com>
2023-04-28 12:12:09 -0600

    4.4: funciones auxiliares y pruebas unitarias, paso 2
    Se definió nueva funcion totalLikes, que devuelve la suma total de likes en todas las publicaciones del blog.

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,17 @@
 const dummy = (blogs) => {
     return 1
-  }
-  
-  module.exports = {
-    dummy
-  }
+}
+
+const totalLikes = (blogs) => {
+    console.log('Tests')
+    const likes=blogs.reduce((sum,blog)=> sum+blog.likes,0)
+    console.log('Total number of likes of array:',likes)
+    return(
+        likes
+    )
+}
+
+module.exports = {
+    dummy,
+    totalLikes
+}
