2e82533ad5d88aca21f6d4d2cfeba3a4944ef01b
413C5 <a_gonzalezzarate2@yahoo.com>
2023-04-28 14:49:27 -0600

    4.5: funciones auxiliares y pruebas unitarias, paso 3
    Se definió una función que recibe una lista de blogs como parámetro, para saber cual blog es el que tiene más likes. Se hace uso de toEqual.

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -3,15 +3,30 @@ const dummy = (blogs) => {
 }
 
 const totalLikes = (blogs) => {
-    console.log('Tests')
-    const likes=blogs.reduce((sum,blog)=> sum+blog.likes,0)
-    console.log('Total number of likes of array:',likes)
-    return(
+    console.log('Tests of sum of likes')
+    const likes = blogs.reduce((sum, blog) => sum + blog.likes, 0)
+    console.log('Total number of likes of array:', likes)
+    return (
         likes
+    )
+}
+
+const favouriteBlog = (blogs) => {
+    console.log('Test of entry with highest n of likes')
+    const favourite = blogs.reduce((previous, actual) => {
+        if (previous.likes > actual.likes)
+            return previous
+        else
+            return actual
+    })
+    console.log('Favourite blog has ',favourite)
+    return (
+        favourite
     )
 }
 
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favouriteBlog
 }
