7751e13a11dac3f66485f6987e40375d0333a2da
413C5 <a_gonzalezzarate2@yahoo.com>
2023-05-01 15:41:35 -0600

    4.9.- Pruebas de lista de blogs, paso 2
    Se hizo prueba que verifica que la propiedad del identificador unico de los blogs se llame id. Del mismo modo se incluyeron describres
    
    -Para que solo se pruebe lo nuevo se usa test.only

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -2,6 +2,7 @@ const supertest = require('supertest')
 const mongoose = require('mongoose')
 const helper = require('./test_helper')
 const app = require('../app')
+const logger = require('../utils/logger')
 const api = supertest(app)
 
 const Blog = require('../models/blog')
@@ -11,99 +12,130 @@ beforeEach(async () => {
   await Blog.insertMany(helper.initialBlogs)
 })
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+describe('1.-Blogs are returned correctly:', () => {
+
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+  })
+
+  test('a specific blog is within the returned blogs', async () => {
+    const response = await api.get('/api/blogs')
+    const titles = response.body.map(r => r.title)
+
+    expect(titles).toContain(
+      'Canonical string reduction')
+  })
+
+  test.only('unique identifier property of the blog posts is named id', async () => {
+    const blogs = await helper.blogsInDb()
+    const blogKeys = Object.keys(blogs[0])
+    const blogIds = blogs.map(r => r.id)
+
+    expect(blogKeys).toContain('id')
+    expect(blogKeys).not.toContain('_id')
+    expect(blogIds[0]).toBeDefined()
+  })
 })
 
 
-test('all blogs are returned', async () => {
-  const response = await api.get('/api/blogs')
+describe('2.-Viewing blogs:', () => {
 
-  expect(response.body).toHaveLength(helper.initialBlogs.length)
+  test('a specific blog can be viewed', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToView = blogsAtStart[0]
+
+    const resultBlog = await api
+      .get(`/api/blogs/${blogToView.id}`)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    const processedBlogToView = JSON.parse(JSON.stringify(blogToView))
+    expect(resultBlog.body).toEqual(processedBlogToView)
+  })
+
+})
+
+describe('3.-Adding blogs:', () => {
+
+  test('a valid blog can be added ', async () => {
+    const newBlog = {
+      title: 'Type wars',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+      likes: 2
+    }
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+
+    const titles = blogsAtEnd.map(r => r.title)
+    expect(titles).toContain(
+      'Type wars')
+  })
+
+  test('blog without title is not added', async () => {
+    const newBlog = {
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+      likes: 10,
+    }
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+  })
+
 })
 
 
-test('a specific blog is within the returned blogs', async () => {
-  const response = await api.get('/api/blogs')
-  const titles = response.body.map(r => r.title)
+describe('4.-Deleting blogs:', () => {
 
-  expect(titles).toContain(
-    'Canonical string reduction')
+  test('a blog can be deleted', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToDelete = blogsAtStart[0]
+
+    await api
+      .delete(`/api/blogs/${blogToDelete.id}`)
+      .expect(204)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(
+      helper.initialBlogs.length - 1)
+
+    const titles = blogsAtEnd.map(r => r.title)
+    expect(titles).not.toContain(blogToDelete.title)
+  })
 })
 
 
-test('a valid blog can be added ', async () => {
-  const newBlog = {
-    title: 'Type wars',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
-    likes: 2
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
-
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
-
-  const titles = blogsAtEnd.map(r => r.title)
-  expect(titles).toContain(
-    'Type wars')
-})
 
 
-test('blog without title is not added', async () => {
-  const newBlog = {
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
-    likes: 10,
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
-
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
-})
 
 
-test('a specific blog can be viewed', async () => {
-  const blogsAtStart = await helper.blogsInDb()
-  const blogToView = blogsAtStart[0]
-
-  const resultBlog = await api
-    .get(`/api/blogs/${blogToView.id}`)
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-
-  const processedBlogToView = JSON.parse(JSON.stringify(blogToView))
-  expect(resultBlog.body).toEqual(processedBlogToView)
-})
 
 
-test('a blog can be deleted', async () => {
-  const blogsAtStart = await helper.blogsInDb()
-  const blogToDelete = blogsAtStart[0]
 
-  await api
-    .delete(`/api/blogs/${blogToDelete.id}`)
-    .expect(204)
 
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd).toHaveLength(
-    helper.initialBlogs.length - 1)
 
-  const titles = blogsAtEnd.map(r => r.title)
-  expect(titles).not.toContain(blogToDelete.title)
-})
+
+
 
 afterAll(async () => {
   await mongoose.connection.close()
