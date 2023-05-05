0f98f394ceac25c8ee7fa092648910ee8abe3908
413C5 <a_gonzalezzarate2@yahoo.com>
2023-05-05 00:16:45 -0600

    4.20.-Expansión de la lista de blogs, paso 8
    Se uso el middleware con tokenExtractor

--- utils/middleware.js
+++ utils/middleware.js
@@ -1,4 +1,6 @@
 const logger = require('./logger')
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -8,9 +10,21 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization')
+
+  request.token = null
+
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    request.token = authorization.substring(7)
+  }
+
+  next()
+}
+
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ 
-    error: 'unknown endpoint' 
+  response.status(404).send({
+    error: 'unknown endpoint'
   })
 }
 
@@ -29,12 +43,23 @@ const errorHandler = (error, request, response, next) => {
   else if (error.name === 'ValidationError')
     return response.status(400).json({ error: error.message })
 
+  else if (error.name === 'JsonWebTokenError') {
+    return response.status(401).json({
+      error: 'invalid token'
+    })
+  } else if (error.name === 'TokenExpiredError') {
+    return response.status(401).json({
+      error: 'token expired'
+    })
+  }
+
 
   next(error)
 }
 
 module.exports = {
   requestLogger,
+  tokenExtractor,
   unknownEndpoint,
   unknownPath,
   errorHandler
