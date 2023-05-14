ba62d58885c5d58bcc03e919f38198be7c6b6955
413C5 <a_gonzalezzarate2@yahoo.com>
2023-05-13 20:09:44 -0600

    5.17-5.22.-Actualización Cypress
    Se actualizo para hacer uso de Cypress

--- app.js
+++ app.js
@@ -32,9 +32,14 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 app.use(middleware.tokenExtractor)
 
-app.use('/api/blogs',blogsRouter)
+app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
+
+if (process.env.NODE_ENV === 'test') {
+    const testingRouter = require('./controllers/testing')
+    app.use('/api/testing', testingRouter)
+}
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
